4a3d8d0cced02b7d3bf7b69527720ffcae4e3d81
HubertGlab <hubert.glab@edu.uekat.pl>
2024-03-16 09:44:15 +0100

    funkcje transpozycja, wyznaczenie_atrybutu

--- Zaj1-3/main.py
+++ Zaj1-3/main.py
@@ -1,18 +1,26 @@
-import test as ts
+import functions as fc
 
 with open('gielda.txt', 'r') as file:
     data = [line.strip().split(',') for line in file]
 
-transposed_data = [list(row) for row in zip(*data)]
 
-count_data = ts.counting(transposed_data)
-attribute_count_values = count_data[0] # dla innych atrybutów trzeba zmienić wartosc w count data
-attribute_count_decision_values = ts.counting_decision_attribiute(transposed_data[0], transposed_data)
-# dla innych atrybutów trzeba zmienić wartosc w transposed_data
+def transpozycja(data):
+    transposed_data = [list(row) for row in zip(*data)]
+    return transposed_data
 
-print(f'entropia dla klasy decyzyjnej wynosi: {ts.decision_entropy(transposed_data)}')
 
+def wyznaczenie_atrybutu(x: int, data):  # x - indeks argumentu, data - wynik przygotowania danych
+    count_data = fc.counting(data)
+    attribute_count_values = count_data[x]
+    attribute_count_decision_values = fc.counting_decision_attribiute(data[x], data)
+    return attribute_count_values, attribute_count_decision_values, x + 1
+
+
+dane = transpozycja(data)
+count_att3, decision_att3, ind = wyznaczenie_atrybutu(2, dane)
+
+print(f'entropia dla klasy decyzyjnej wynosi: {fc.decision_entropy(dane)}')
 print(f'wartosc funkcji informacji wynosi:'
-      f' {ts.info(attribute_count_values, attribute_count_decision_values)} dla atrybutu nr 1')
+      f' {fc.info(count_att3, decision_att3)} dla atrybutu nr {ind}')
 print(f'wartosc przyrostu informacji wynosi:'
-      f' {ts.info_gain(attribute_count_values, attribute_count_decision_values, transposed_data)} dla atrybutu nr 1')
+      f' {fc.info_gain(count_att3, decision_att3, dane)} dla atrybutu nr {ind}')

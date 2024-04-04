7bda564463ef279753ee4e50ef3a57fe01d24ca8
HubertGlab <hubert.glab@edu.uekat.pl>
2024-04-04 12:23:54 +0200

    decision tree

--- Zaj1-3/functions.py
+++ Zaj1-3/functions.py
@@ -1,7 +1,8 @@
 import math
 
+
 # zliczanie wystąpien elementow danego argumentu np dla a1 old = 3, mid = 4, new = 3
-def counting(data):
+def counting_all(data):
     i = 0
     data_count = {}
     for row in data:
@@ -14,6 +15,7 @@ def counting(data):
         i += 1
     return data_count
 
+
 # entropia podajemy wynik counting
 def entropy(attribute: dict):
     total = sum(attribute.values())
@@ -24,13 +26,15 @@ def entropy(attribute: dict):
             entropy_val -= probability * math.log2(probability)
     return entropy_val
 
+
 def decision_entropy(data):
-    last_row = counting(data)
+    last_row = counting_all(data)
     last_row = last_row[len(last_row) - 1]
     return entropy(last_row)
 
-#zliczanie kombinacji elementow atrybutu oraz elementow decyzji np. dla a1 old, down : 3, old, up: 0 (nie istnieje)
-#podajemy atrybut (transposed data) oraz wykorzystywane dane
+
+# zliczanie kombinacji elementow atrybutu oraz elementow decyzji np. dla a1 old, down : 3, old, up: 0 (nie istnieje)
+# podajemy atrybut (transposed data) oraz wykorzystywane dane
 def counting_decision_attribiute(transposed_data, data):
     count_decision_attribiute = {}
     last_row = data[-1]
@@ -51,7 +55,8 @@ def counting_decision_attribiute(transposed_data, data):
         count_decision_attribiute[key] = help_dict
     return count_decision_attribiute
 
-#liczenie funkcji informacji podajemy wynik counting oraz counting_decision_attribute
+
+# liczenie funkcji informacji podajemy wynik counting oraz counting_decision_attribute
 def info(attribute, count_decision):
     total = sum(attribute.values())
     info_val = 0
@@ -61,21 +66,42 @@ def info(attribute, count_decision):
         info_val += probability * entropy_val
     return info_val
 
-#wyliczanie przyrostu informacji
+
+# wyliczanie przyrostu informacji
 def info_gain(attribute, count_decision, data):
-    last_row = counting(data)
+    last_row = counting_all(data)
     last_row = last_row[len(last_row) - 1]
     return entropy(last_row) - info(attribute, count_decision)
+
 
 def balanced_information_growth(attribute, count_decision, data):
     splitinfo = entropy(attribute)
     gain = info_gain(attribute, count_decision, data)
-    return gain/splitinfo
+    if splitinfo != 0:
+        return gain / splitinfo
+    else:
+        return 0
 
 
+def wyznaczenie_atrybutu(x: int, data):  # x - indeks argumentu, data - wynik przygotowania danych
+    count_data = counting_all(data)
+    attribute_count_values = count_data[x]
+    attribute_count_decision_values = counting_decision_attribiute(data[x], data)
+    return attribute_count_values, attribute_count_decision_values, x + 1
+#funkcja zwraca wystapienia elementow danego atrybutu, par elementow atrybutu i elementow atrybutu decyzyjnego oraz index
 
+def best_info_gain(dane):
+    count_att, decision_att, ind = wyznaczenie_atrybutu(0, dane)
+    balanced_info_val_1 = balanced_information_growth(count_att, decision_att, dane)
+    saved_i = 0
+    for i in range(0, len(dane) - 1):
+        count_att, decision_att, ind = wyznaczenie_atrybutu(i, dane)
+        balanced_info_val_2 = balanced_information_growth(count_att, decision_att, dane)
+        if balanced_info_val_2 >= balanced_info_val_1:
+            balanced_info_val_1 = balanced_info_val_2
+            saved_i = i
+    return balanced_info_val_1, saved_i
 
-
-
-
-
+def transpozycja(data):
+    transposed_data = [list(row) for row in zip(*data)]
+    return transposed_data
--- Zaj1-3/main.py
+++ Zaj1-3/main.py
@@ -3,26 +3,27 @@ import functions as fc
 with open('gielda.txt', 'r') as file:
     data = [line.strip().split(',') for line in file]
 
+dane = fc.transpozycja(data)
+atrybuty = dane[:len(dane) - 1]
+count_att, decision_att, ind = fc.wyznaczenie_atrybutu(0, dane)
+# for item in dane:
+#     print(dane.index(item))
+# print(f'entropia dla klasy decyzyjnej wynosi: {fc.decision_entropy(dane)}')
+# print(f'wartosc funkcji informacji wynosi:'
+#       f' {fc.info(count_att, decision_att)} dla atrybutu nr {ind}')
+# print(f'wartosc przyrostu informacji wynosi:'
+#       f' {fc.info_gain(count_att, decision_att, dane)} dla atrybutu nr {ind}')
+# print(f'zrownowazony przyrost informacji wynosi:'
+#       f' {fc.balanced_information_growth(count_att, decision_att, dane)} dla atrybutu nr {ind}')
+# for i in range(0, len(dane) - 1):
+#     count_att, decision_att, ind = fc.wyznaczenie_atrybutu(i, dane)
+#     print(f'zrownowazony przyrost informacji wynosi:'
+#        f' {fc.balanced_information_growth(count_att, decision_att, dane)} dla atrybutu nr {ind}')
 
-def transpozycja(data):
-    transposed_data = [list(row) for row in zip(*data)]
-    return transposed_data
+# best_gain, index = fc.best_info_gain(dane)
+# best_gain_att = dane[index]
+# print(f'najlepszy przyrost informacji o wartości {best_gain}, występuje dla atrybutu o indeksie: {index}')
 
 
-def wyznaczenie_atrybutu(x: int, data):  # x - indeks argumentu, data - wynik przygotowania danych
-    count_data = fc.counting(data)
-    attribute_count_values = count_data[x]
-    attribute_count_decision_values = fc.counting_decision_attribiute(data[x], data)
-    return attribute_count_values, attribute_count_decision_values, x + 1
 
 
-dane = transpozycja(data)
-count_att, decision_att, ind = wyznaczenie_atrybutu(0, dane)
-
-print(f'entropia dla klasy decyzyjnej wynosi: {fc.decision_entropy(dane)}')
-print(f'wartosc funkcji informacji wynosi:'
-      f' {fc.info(count_att, decision_att)} dla atrybutu nr {ind}')
-print(f'wartosc przyrostu informacji wynosi:'
-      f' {fc.info_gain(count_att, decision_att, dane)} dla atrybutu nr {ind}')
-print(f'zrownowazony przyrost informacji wynosi:'
-      f' {fc.balanced_information_growth(count_att, decision_att, dane)} dla atrybutu nr {ind}')

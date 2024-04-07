ee01270944fa12566684eee1ab2fd4a8debb1998
HubertGlab <hubert.glab@edu.uekat.pl>
2024-04-07 14:24:01 +0200

    finalna werjsa

--- Zaj1-3/classes.py
+++ Zaj1-3/classes.py
@@ -1,8 +1,5 @@
 class Node:
-    def __init__(self,attribute_index = None, attribute_value = None, decision = None, children = None):
+    def __init__(self,attribute_index = None, decision = None, children = None):
         self.attribute_index = attribute_index
-        self.attribute_value = attribute_value
         self.decision = decision
         self.children = children if children is not None else {}
-    # def __str__(self):
-    #     return f'{self.attribute_value}, {self.decision}, {self.children}'
